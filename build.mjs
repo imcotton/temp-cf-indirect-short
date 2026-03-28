e8a12a7d6dad4cefde068a5fa27ef3c35409edda
Cotton Hou <himcotton@gmail.com>
2026-03-29 01:39:45 +0800

    add `cloudflare:workers` as `external` of esbuild config

--- build.mjs
+++ build.mjs
@@ -12,6 +12,7 @@ await esbuild.build({
     format: 'esm',
     platform: 'node',
     target: [ 'node20', 'es2022' ],
+    external: [ 'cloudflare:workers' ],
     plugins: [ esbuild_plugin ],
     legalComments: 'none',
     outfile: 'bin.js',
